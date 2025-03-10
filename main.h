8cc8044e3c574b475f07132c11b9bddea67221bd
João Fassina <158244312+jf-fassina@users.noreply.github.com>
2025-03-10 11:54:36 -0300

    Add files via upload

--- main.h
+++ main.h
@@ -1,3 +1,9 @@
 #include <zip.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+//ERROR CHECKPOINTS BECAUS YO SOY MACACO
+#define CHCK_SUCCESS "FULL BOX, 200"
+#define CHCK_ERROR "SADDAM HUSSEIN HIDING SPOT FOUND"
+
+
